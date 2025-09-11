c97e087f4ce4a3032a86db212d9b46d40c93a9d5
stkisengese <skisengese@gmail.com>
2025-09-11 21:01:04 +0300

    feat: Add initial server implementation

---
+++ cmd/server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/stkisengese/B2B-Data-Platform/internal/config"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(fmt.Sprintf(":%d", cfg.Server.Port))
+}
