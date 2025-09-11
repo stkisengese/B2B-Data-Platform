0bf7c1f23f3b8ab197b08868323f7b85ff394748
stkisengese <skisengese@gmail.com>
2025-09-11 21:01:13 +0300

    feat: Add viper for configuration management

---
+++ internal/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server   ServerConfig
+	Database DatabaseConfig
+}
+
+type ServerConfig struct {
+	Port int
+}
+
+type DatabaseConfig struct {
+	Path string
+}
+
+func LoadConfig() (config Config, err error) {
+	viper.AddConfigPath("./internal/config")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
