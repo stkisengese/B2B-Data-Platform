8a082633c4b29a659b1ef426e321833637cf92d7
stkisengese <skisengese@gmail.com>
2025-09-12 00:00:57 +0300

    feat: Implement database schema and migrations for companies and addresses

---
+++ migrations/000003_add_indexes.down.sql
@@ -0,0 +1,9 @@
+DROP INDEX idx_companies_name;
+DROP INDEX idx_companies_industry;
+DROP INDEX idx_companies_data_source;
+
+DROP INDEX idx_addresses_company_id;
+DROP INDEX idx_addresses_city;
+DROP INDEX idx_addresses_state;
+DROP INDEX idx_addresses_country;
+DROP INDEX idx_addresses_postal_code;
