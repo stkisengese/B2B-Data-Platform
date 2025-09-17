5129177627bd31940594e908e213e69b7a0fd295
stkisengese <skisengese@gmail.com>
2025-09-17 23:50:48 +0300

    feat: Add DataSourcesConfig and CompaniesHouseConfig to configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,8 +5,9 @@ import (
 )
 
 type Config struct {
-	Server   ServerConfig
-	Database DatabaseConfig
+	Server      ServerConfig
+	Database    DatabaseConfig
+	DataSources DataSourcesConfig
 }
 
 type ServerConfig struct {
@@ -17,10 +18,35 @@ type DatabaseConfig struct {
 	Path string
 }
 
+type DataSourcesConfig struct {
+	// OpenCorporates OpenCorporatesConfig
+	CompaniesHouse CompaniesHouseConfig
+}
+
+// type OpenCorporatesConfig struct {
+// 	APIKey   string
+// 	Enabled  bool
+// 	RateLimit int
+// }
+
+type CompaniesHouseConfig struct {
+	APIKey    string
+	Enabled   bool
+	RateLimit int
+}
+
 func LoadConfig() (config Config, err error) {
 	viper.AddConfigPath("./internal/config")
 	viper.SetConfigName("config")
 	viper.SetConfigType("yml")
+
+	// Set defaults
+	viper.SetDefault("server.port", 8080)
+	viper.SetDefault("database.path", "b2b.db")
+	// viper.SetDefault("datasources.opencorporates.enabled", false)
+	// viper.SetDefault("datasources.opencorporates.ratelimit", 5)
+	viper.SetDefault("datasources.companieshouse.enabled", false)
+	viper.SetDefault("datasources.companieshouse.ratelimit", 5)
 
 	viper.AutomaticEnv()
 
