0841c2a005687d2e7b2ab47faefb505c5a0ecadc
stkisengese <skisengese@gmail.com>
2025-10-17 01:43:25 +0300

    chore: add documentation comments

--- internal/database/database.go
+++ internal/database/database.go
@@ -8,7 +8,9 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// NewDatabaseConnection establishes a new database connection to the SQLite database
 func NewDatabaseConnection(path string) (*sqlx.DB, error) {
+	// Use WAL journal mode for better concurrency performance with SQLite
 	db, err := sqlx.Connect("sqlite3", fmt.Sprintf("file:%s?_journal_mode=WAL", path))
 	if err != nil {
 		return nil, err
